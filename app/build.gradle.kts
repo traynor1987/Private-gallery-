a9767392906fe3358f2a5049188374360334ef16
James Traynor <Jtraynor1987@aol.com>
2026-09-20 21:21:41 +0100

    build: advance release version to 1.0.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "uk.co.traynor.privategallery"
     minSdk = 26
     targetSdk = 36
-    versionCode = 23
-    versionName = "1.0.22"
+    versionCode = 24
+    versionName = "1.0.23"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
   buildFeatures { compose = true; buildConfig = true }
